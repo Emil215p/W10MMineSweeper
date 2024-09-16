ec5b9a27e1d1f918571260ae81a5d82b91a44e8b
Emil215p <juelemil@gmail.com>
2024-09-16 23:50:59 +0200

    Added more buttons, added UI.

--- W10MMineSweeper/MainPage.xaml.cs
+++ W10MMineSweeper/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -13,18 +15,50 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace W10MMineSweeper
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MineSweeperPage));
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            CoreApplication.Exit();
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HelpButton_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayHelpDialog();
+        }
+        private async void DisplayHelpDialog()
+        {
+            ContentDialog HelpDialog = new ContentDialog
+            {
+                Title = "To play",
+                Content = "Click start and then click on a field, to mark a mine long click, if unsure if its a mine long click again.",
+                CloseButtonText = "OK"
+            };
+
+            ContentDialogResult result = await HelpDialog.ShowAsync();
+        }
+
+        private void CreditsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(CreditsPage));
         }
     }
 }
