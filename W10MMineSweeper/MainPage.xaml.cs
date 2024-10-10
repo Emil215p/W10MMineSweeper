1af753c9c15440551fd44109a22535569f8454fb
Emil215p <juelemil@gmail.com>
2024-10-10 08:37:31 +0200

    Background, increased readability of text.

--- W10MMineSweeper/MainPage.xaml.cs
+++ W10MMineSweeper/MainPage.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel.Core;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace W10MMineSweeper
 {
@@ -22,6 +12,16 @@ namespace W10MMineSweeper
     {
         public MainPage()
         {
+
+            Uri imageUri = new Uri("ms-appx:///Assets/background.png");
+            BitmapImage bitmapImage = new BitmapImage(imageUri);
+            ImageBrush imageBrush = new ImageBrush
+            {
+                ImageSource = bitmapImage
+            };
+
+            Application.Current.Resources["ApplicationPageBackgroundThemeBrush"] = imageBrush;
+
             this.InitializeComponent();
         }
 
