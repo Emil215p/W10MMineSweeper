6144a0330a0a2be04c0010b8a2a242b5fbfa16a3
Emil215p <juelemil@gmail.com>
2024-10-11 11:20:15 +0200

    You now get prompted to ask for gridsize when clicking play.

--- W10MMineSweeper/MineSweeperPage.xaml.cs
+++ W10MMineSweeper/MineSweeperPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -25,6 +26,84 @@ namespace W10MMineSweeper
         public MineSweeperPage()
         {
             this.InitializeComponent();
+            _ = GridSizeDialogAsync();
+        }
+
+
+        private async Task GridSizeDialogAsync()
+        {
+            // Create the ContentDialog
+            ContentDialog inputDialog = new ContentDialog()
+            {
+                Title = "Set Grid Size",
+                PrimaryButtonText = "OK",
+                SecondaryButtonText = "Cancel",
+                Content = new StackPanel
+                {
+                    Children =
+            {
+                new TextBox
+                {
+                    Name = "gridSizeTextBox",
+                    Header = "Grid Size",
+                    PlaceholderText = "Enter grid size...",
+                    InputScope = new InputScope
+                    {
+                        Names = { new InputScopeName { NameValue = InputScopeNameValue.Number } }
+                    }
+                }
+            }
+                }
+            };
+
+            // Show the dialog and wait for the result asynchronously
+            ContentDialogResult result = await inputDialog.ShowAsync();
+
+            if (result == ContentDialogResult.Primary)
+            {
+                var textBox = (TextBox)((StackPanel)inputDialog.Content).Children[0];
+                if (int.TryParse(textBox.Text, out int gridSize))
+                {
+                    InitializeGrid(gridSize);
+                    AddBordersToGrid(gridSize);
+                }
+                else
+                {
+                    InitializeGrid(10); // Default value if parsing fails
+                    AddBordersToGrid(10);
+                }
+            }
+        }
+
+
+
+        private void AddBordersToGrid(int gridSize)
+        {
+            SweepGrid.Children.Clear();
+            for (int row = 0; row < gridSize; row++)
+            {
+                for (int col = 0; col < gridSize; col++)
+                {
+                    Border border = new Border
+                    {
+                        BorderBrush = new SolidColorBrush(Windows.UI.Colors.Black),
+                        BorderThickness = new Thickness(1)
+                    };
+                    Grid.SetRow(border, row);
+                    Grid.SetColumn(border, col);
+                    SweepGrid.Children.Add(border);
+                }
+            }
+        }
+        private void InitializeGrid(int gridSize)
+        {
+            SweepGrid.ColumnDefinitions.Clear();
+            SweepGrid.RowDefinitions.Clear();
+            for (int i = 0; i < gridSize; i++)
+            {
+                SweepGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                SweepGrid.RowDefinitions.Add(new RowDefinition());
+            }
         }
 
         private void ReturnButton_Click(object sender, RoutedEventArgs e)
@@ -34,7 +113,7 @@ namespace W10MMineSweeper
 
         private void RestartButton_Click(object sender, RoutedEventArgs e)
         {
-            
+
         }
     }
 }
